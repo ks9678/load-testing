ad27ce63660410e7981becfaaf5c445aff7d9510
ks9678 <98112324+ks9678@users.noreply.github.com>
2022-05-04 14:47:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
-
+import { check, group } from 'k6';
+ 
+export let options = {
+   stages: [
+       { duration: '0.5m', target: 3 }, // simulate ramp-up of traffic from 1 to 3 virtual users over 0.5 minutes.
+       { duration: '0.5m', target: 4}, // stay at 4 virtual users for 0.5 minutes
+       { duration: '0.5m', target: 0 }, // ramp-down to 0 users
+     ],
+};
+ 
 export default function () {
-  http.get('https://test.k6.io');
-  sleep(1);
-}
+   group('API uptime check', () => {
+       const response = http.get('https://todo-app-barkend.herokuapp.com/todos/');
+       check(response, {
+           "status code should be 200": res => res.status === 200,
+       });
+   });
+ 
+   let todoID;
+   group('Create a Todo', () => {
+       const response = http.post('https://todo-app-barkend.herokuapp.com/todos/',
+       {"task": "write k6 tests"}
+       );
+       todoID = response.json()._id;
+       check(response, {
+           "status code should be 200": res => res.status === 200,
+       });
+       check(response, {
+           "response should have created todo": res => res.json().completed === false,
+          });
+        })
+};
